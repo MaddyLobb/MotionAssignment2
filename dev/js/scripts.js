e8f3f9882b212e30ebbd4f551ff3c8e319c2089a
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2021-11-08 18:26:41 -0500

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -20,9 +20,12 @@ function jars(){
 
     tl.from("#jars",{duration:0.001, alpha:0})
       .from("#jars",{duration:.75, drawSVG: "0%", ease: "none"})
-      .to("#jars" ,{duration: .25, stroke: "#E9E5D8", scaleX: 1.1, scaleY: 1.1, transformOrigin: "50% 50%", ease: "expo.in"})
-      .to("#jars" ,{duration: .25, stroke: "#E9E5D8", scaleX: 1, scaleY: 1, transformOrigin: "50% 50%", ease: "expo.in"})
-      .fromTo("#jars", {drawSVG:"0% 100%"},{duration: .75, drawSVG:"100% 100%", ease: "slow (0.7, 0.7, false)"});
+      .from("#lid1" ,{duration: .05, alpha: 0, rotate: 0}, 0.3)
+      .from("#lid2" ,{duration: .05, alpha:0, rotate: 0}, 0.49)
+      .from(".lids", {duration: .5, rotate: -20, transformOrigin: "0% 0"})
+      .fromTo("#jars", {drawSVG:"0% 100%"},{duration: .75, drawSVG:"100% 100%", ease: "slow (0.7, 0.7, false)"})
+      .to("#lid1" ,{duration: .05, alpha: 0}, 1.42)
+      .to("#lid2" ,{duration: .05, alpha:0}, 1.55);
 
     return tl;
 }
@@ -38,10 +41,10 @@ function mixer(){
       .from("#blade1", {duration: .125, alpha:0}, 0.335)
       .from("#blade2", {duration: .125, alpha:0}, 0.322)
       .to("#blade1", {duration: .25, motionPath: {path:"#path1", align: "#path1"},repeat:1},"same")
-      .to("#blade2", {duration: .25, motionPath: {path:"#path2", align: "#path2"},repeat:1},"same")
-      .to("#blade1", {duration: .25, alpha:0})
-      .to("#blade2", {duration: .25, alpha:0})
-      .fromTo("#mixer", {drawSVG:"0% 100%"},{duration: .75, drawSVG:"100% 100%", ease: "slow (0.7, 0.7, false)"});
+      .from("#blade2", {duration: .25, motionPath: {path:"#path2", align: "#path2"},repeat:1},"same")
+      .fromTo("#mixer", {drawSVG:"0% 100%"},{duration: .75, drawSVG:"100% 100%", ease: "slow (0.7, 0.7, false)"})
+      .to("#blade1", {duration: .125, alpha:0}, 1.43)
+      .to("#blade2", {duration: .125, alpha:0}, 1.42);
 
     return tl;
 }
@@ -53,20 +56,20 @@ function oven(){
 
     tl.from("#oven",{duration:0.001, alpha:0})
       .from("#oven",{duration: .75, drawSVG: "0%", ease: "none"})
-      .from("#knob1",{duration: .005, alpha:0}, 0.13)
-      .from("#knob2",{duration: .005, alpha:0}, 0.135)
-      .from("#knob3",{duration: .005, alpha:0, rotate: 0}, 0.139)
-      .from("#knob4",{duration: .005, alpha:0}, 0.145)
-      .from("#knob5",{duration: .005, alpha:0}, 0.149)
+      .from("#knob1",{duration: .005, alpha:0}, 0.34)
+      .from("#knob2",{duration: .005, alpha:0}, 0.35)
+      .from("#knob3",{duration: .005, alpha:0, rotate: 0}, 0.36)
+      .from("#knob4",{duration: .005, alpha:0}, 0.37)
+      .from("#knob5",{duration: .005, alpha:0}, 0.38)
       .from("#knob3", {duration: .25, rotate: -10, transformOrigin: "0% 0"})
-      .to("#oven", { duration: .25, stroke: "#FF522D", strokeWidth: 5})
+      .to("#oven", { duration: .5, stroke: "#FF522D", strokeWidth: 5})
       .to("#oven", { duration: .25, stroke: "#000", strokeWidth: 3})
       .fromTo("#oven", {drawSVG:"0% 100%"},{duration: .75, drawSVG:"100% 100%", ease: "slow (0.7, 0.7, false)"})
-      .to("#knob1",{duration: .005, alpha:0}, 1.375)
-      .to("#knob2",{duration: .005, alpha:0}, 1.38)
-      .to("#knob3",{duration: .005, alpha:0, rotate: 0}, 1.383)
-      .to("#knob4",{duration: .005, alpha:0}, 1.389)
-      .to("#knob5",{duration: .005, alpha:0}, 1.394);
+      .to("#knob1",{duration: .005, alpha:0}, 1.94)
+      .to("#knob2",{duration: .005, alpha:0}, 1.95)
+      .to("#knob3",{duration: .005, alpha:0, rotate: 0}, 1.96)
+      .to("#knob4",{duration: .005, alpha:0}, 1.97)
+      .to("#knob5",{duration: .005, alpha:0}, 1.98);
 
     return tl;
 }
@@ -78,35 +81,41 @@ function cake(){
 
     tl.from("#cake",{duration:0.001, alpha:0})
       .from("#cake",{duration:.75, drawSVG: "0%", ease: "none"})
-      .from("#candle", {duration: .25, alpha: 0})
+      .from("#candle", {duration: .5, alpha: 0})
       .fromTo("#cake", {drawSVG:"0% 100%"},{duration: .75, drawSVG:"100% 100%", ease: "slow (0.7, 0.7, false)"})
-      .to("#candle", { duration: .25, alpha: 0, ease: "expo.out"}, "<-= 70%");
+      .to("#candle", { duration: .25, alpha: 0, ease: "expo.out"}, "<-= 70%")
+      .to("#preloader", { duration: .25, alpha: 0, onComplete: removepreloader});
 
     return tl;
 }
 
+function removepreloader(){
+
+  window.scrollTo(0,0);
+
+  gsap.set("#preloader", {display: "none"});
+
+}
+
+function heroanimation(){
+
+    const tl = gsap.timeline ();
+
+    tl.from("#pathOne",{duration:2, drawSVG: "0%"})
+      .to("#pathOne",{duration: 0.05, alpha: 0})
+      .from("#text",{duration:3, alpha: 0},">-.5")
+      .from("#header", {duration: .5, alpha: 0},">-2");
+
+    return tl;
+
+}
 
 
 mainTL.add(jars())
-      .add(mixer())
-      .add(oven())
-      .add(cake());
-
-
-
-
-
-//Hero Animation -------------------------------------------------------------------------------------------
-// pathOne or circle starts at begining of timeline and will draw out fully then disapear
-// mainTL.to("#pathOne",{duration: 2, drawSVG:"0%"});
-
-
-// hero text will starts at end of the timeline and will fade onto screen -1 second before the end of the previous animation of a circle and last 2 seconds
-// mainTL.from("#text",{duration:3, alpha: 0},">-1");
-
-// header or navbar text starts at end of the timeline and will fade onto screen -2 seconds before the end of the previous animation of hero text and will last .5 seconds 
-// mainTL.from("#header", {duration: .5, alpha: 0},">-2");
-
+      .add(mixer(), "-=.5")
+      .add(oven(), "-=.5")
+      .add(cake(), "-=.5")
+      .add(heroanimation)();
 
 
 GSDevTools.create();
